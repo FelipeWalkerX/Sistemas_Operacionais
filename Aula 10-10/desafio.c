d1a073389c5059c712eb03874172eeb56cb69e37
Allan Felipe <113111468+FelipeWalkerX@users.noreply.github.com>
2024-10-10 11:43:00 -0300

    Update desafio.c

--- Aula 10-10/desafio.c
+++ Aula 10-10/desafio.c
@@ -12,23 +12,24 @@ int meuLen(char *str){  //Funcao para contar as letras de cada frase
 }
 
 void saiPrograma (int signal){  //funcao para sair do programa
-    int sair;
+    char sair;
     contador++; //Apenas conta quantas vezes o usuario apertou o ctrl C
 
     if (contador == 3)    { //Se o usuario apertar ctrl C 3 vezes, ele entra no if
         write(1, "Deseja realmente sair? [Y/n]: \n\n", meuLen("Deseja realmente sair? [Y/n]: \n\n"));
         alarm(20);  //cooldown de 20 segundos para o usuario escolher continuar ou nao com o programa
         read(0, &sair, sizeof(int));    //le a resposta do usuario
-        if (sair = 'y')
+        if (sair == 'y')
         {
-            write(1, "Saindo......", meuLen("Saindo......"));   //Usuario digitou y, entao ele sai do programa
+            write(1, "Saindo...... \n", meuLen("Saindo...... \n"));   //Usuario digitou y, entao ele sai do programa
             exit(0);    //forcar encerramento do programa
-        } else if (sair = 'n')
+        } else if (sair == 'n')
         {
-            write(1, "Continuando...", meuLen("Continuando..."));   //Usuario digitou n, entao ele continua o programa
+            write(1, "Continuando... \n", meuLen("Continuando... \n"));   //Usuario digitou n, entao ele continua o programa
             contador = 0;
+            alarm(5);
         } else{
-            write(1, "Demorou muito para responder. Saindo......", meuLen("Demorou muito para responder. Saindo......"));       // Se deu os 20 segundos e o usuario nao digitar nada, ele encerra o programa
+            write(1, "Demorou muito para responder. Saindo...... \n", meuLen("Demorou muito para responder. Saindo...... \n"));       // Se deu os 20 segundos e o usuario nao digitar nada, ele encerra o programa
             exit(0);
         }
         
@@ -41,9 +42,9 @@ void saiPrograma (int signal){  //funcao para sair do programa
 void mostraC(int signal){   //Funcao para contar quantas vezes foi apertada o ctrl C
 
     int cont = contador + '0';  // Passando o contador de inteiro para ASCII
-    write(1, "Voce apertou Ctrl-C ", meuLen("Voce apertou Ctrl-C"));
+    write(1, "\n\nVoce apertou Ctrl-C ", meuLen("\n\nVoce apertou Ctrl-C "));
     write(1, &cont, 1); 
-    write(1, "vezes \n\n", meuLen("vezes \n\n"));   //Tem que ter essa quebra de linha, pois em baixo nivel, o sistema nao entende tudo de uma vez
+    write(1, " vezes \n\n", meuLen(" vezes \n\n"));   //Tem que ter essa quebra de linha, pois em baixo nivel, o sistema nao entende tudo de uma vez
 }
 
 void pulsoLRM(int signal){
